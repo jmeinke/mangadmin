f02e2f5a4cc0e7481bdb68946cc1d52a1fe5f2be
josh.developer <josh.developer@ba689e30-f431-0410-9da9-add0f311814c>
2007-07-04 18:14:34 +0000

    - upgraded MangAdmin's License to GPL v.3   - applied GPL v.3 headers to nearly all MangAdmin files   - added a more clearly format to this changelog now each lines maximum is 110 columns   - updated finnish translation, thanks to Lone

--- trunk/Locales/enUS.lua
+++ trunk/Locales/enUS.lua
@@ -1,4 +1,22 @@
--- english by Josh
+-------------------------------------------------------------------------------------------------------------
+--
+-- MangAdmin Version 1.0
+--
+-- Copyright (C) 2007 Free Software Foundation, Inc.
+-- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+-- This is free software: you are free to change and redistribute it.
+-- There is NO WARRANTY, to the extent permitted by law.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--   Official Website    : http://mangadmin.all-mag.de
+-- GoogleCode Website    : http://code.google.com/p/mangadmin/
+-- Subversion Repository : http://mangadmin.googlecode.com/svn/
+--
+-------------------------------------------------------------------------------------------------------------
+
 function Return_enUS()
   return {
     ["slashcmds"] = { "/mangadmin", "/ma" },
