76236a5cb51dfcb047e8539a02878259858f377d
josh.developer <josh.developer@ba689e30-f431-0410-9da9-add0f311814c>
2007-07-19 15:45:46 +0000

    - forgot updating some language files

--- trunk/Locales/frFR.lua
+++ trunk/Locales/frFR.lua
@@ -45,6 +45,9 @@ function Return_frFR()
     ["tt_ItemButton"] = "Click this button to toggle a popup with the function to search for items and manage your favorites.",
     ["tt_SpellButton"] = "Click this button to toggle a popup with the function to search for spells and manage your favorites.",
     ["tt_SearchDefault"] = "Now you can enter a keyword and start the search.",
+    ["tt_AnnounceButton"] = "Click to announce a system message.",
+    ["tt_KickButton"] = "Click to kick the selected player from the server.",
+    ["tt_ShutdownButton"] = "Click to shut down the server in the amount of seconds from the field, if omitted shut down immediately!",
     ["ma_ItemButton"] = "Items",
     ["ma_SpellButton"] = "Spells",
     ["ma_LanguageButton"] = "Change language",
@@ -56,6 +59,9 @@ function Return_frFR()
     ["ma_LearnLangButton"] = "All languages",
     ["ma_LearnClassButton"] = "All class-spells",
     ["ma_SearchButton"] = "Search...",
-    ["ma_ResetButton"] = "Reset"
+    ["ma_ResetButton"] = "Reset",
+    ["ma_KickButton"] = "Kick",
+    ["ma_AnnounceButton"] = "Announce",
+    ["ma_ShutdownButton"] = "Shut down!"
   }
 end
