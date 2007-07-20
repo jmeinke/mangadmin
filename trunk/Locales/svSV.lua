854114c04a502763ee5642ae2e4e12faa0a07a7d
josh.developer <josh.developer@ba689e30-f431-0410-9da9-add0f311814c>
2007-07-20 05:30:10 +0000

    - applied UTF-8 encoding to the language files   - updated Czech language, thanks to Virnik

--- trunk/Locales/svSV.lua
+++ trunk/Locales/svSV.lua
@@ -1,4 +1,4 @@
--------------------------------------------------------------------------------------------------------------
+ï»¿-------------------------------------------------------------------------------------------------------------
 --
 -- MangAdmin Version 1.0
 --
@@ -31,12 +31,12 @@ function Return_svSV()
     ["tabmenu_Server"] = "Server",
     ["tabmenu_Log"] = "Log",
     ["tt_Default"] = "Flytta din muspekare \195\182ver ett element f\195\182r att aktivera tooltipen!",
-    ["tt_MainButton"] = "Tryck på den h\195\164r knappen f\195\182r att visa MangAdmin huvudmeny.",
+    ["tt_MainButton"] = "Tryck pÃ¥ den h\195\164r knappen f\195\182r att visa MangAdmin huvudmeny.",
     ["tt_CharButton"] = "Inte klart.",
     ["tt_TeleButton"] = "Inte klar.",
     ["tt_TicketButton"] = "Inte klart.",
-    ["tt_ServerButton"] = "Tryck på den h\195\164r knappen f\195\182r att \195\182ppna ett f\195\182nster med information om servern på den aktuella realmen.",
-    ["tt_LogButton"] = "Tryck på den h\195\164r knappen f\195\182r att \195\182ppna ett protokoll \195\182ver allting som du/MangAdmin har gjort \195\164n så l\195\164nge.",
+    ["tt_ServerButton"] = "Tryck pÃ¥ den h\195\164r knappen f\195\182r att \195\182ppna ett f\195\182nster med information om servern pÃ¥ den aktuella realmen.",
+    ["tt_LogButton"] = "Tryck pÃ¥ den h\195\164r knappen f\195\182r att \195\182ppna ett protokoll \195\182ver allting som du/MangAdmin har gjort \195\164n sÃ¥ l\195\164nge.",
     ["tt_LanguageButton"] = "Click this button to change the language and reload MangAdmin.",
     ["tt_ToggleGMButton"] = "Click this button to toggle GM-mode.",
     ["tt_ToggleFlyButton"] = "Click this button to toggle Fly-mode",
