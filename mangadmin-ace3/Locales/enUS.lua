84ebff7382b57fcaea839d72119e2621a6a3b98a
josh.developer <josh.developer@ba689e30-f431-0410-9da9-add0f311814c>
2008-06-13 14:56:14 +0000

    * Welcome Home example from Ace3 Wiki

---
+++ mangadmin-ace3/Locales/enUS.lua
@@ -0,0 +1,17 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MangAdmin2", "enUS", true)
+
+-- Chat commands
+L["welcomehome"] = true
+L["wh"] = true
+
+L["Welcome Home!"] = true -- default message
+
+L["Message"] = true
+L["The message to be displayed when you get home."] = true
+L["<Your message>"] = true -- usage
+
+L["Show in Chat"] = true
+L["Toggles the display of the message in the chat window."] = true
+
+L["Show on Screen"] = true
+L["Toggles the display of the message on the screen."] = true
